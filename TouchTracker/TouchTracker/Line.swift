27f1d476addb20248f2855b62c8f875012abfa81
minedamnesia <kelly@climate.com>
2015-10-29 13:47:37 -0700

    add touch response chapter exercise

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Kelly Yamamoto on 10/29/15.
+//  Copyright © 2015 Kelly Yamamoto. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
