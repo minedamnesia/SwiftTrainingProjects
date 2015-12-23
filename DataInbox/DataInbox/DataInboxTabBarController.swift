6d4832e469d5c707e7473320f1806693a7be3265
minedamnesia <kelly@climate.com>
2015-12-22 16:08:22 -0800

    add CanvasLab exercise to swift training exercise folder

---
+++ DataInbox/DataInbox/DataInboxTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  DataInboxTabBarController.swift
+//  DataInbox
+//
+//  Created by Kelly Yamamoto on 11/12/15.
+//  Copyright © 2015 Kelly Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class DataInboxTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
